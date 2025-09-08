f9a98b65eded09730091e6c1d1c6b8db22a59530
junin <juniomoreirabrito@gmail.com>
2025-09-08 19:44:08 -0300

    ola mundo

---
+++ ola mundo-1.py
@@ -0,0 +1,15 @@
+from flask import Flask 
+from dotenv import load_dotenv 
+import os
+
+load_dotenv()
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+        return "Olá, mundo!"
+
+if __name__ == "__main__":
+        app.run(debug=True)
