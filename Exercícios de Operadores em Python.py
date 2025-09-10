ec64b68af19f1f252ade3e5973674b22175eb816
junin <juniomoreirabrito@gmail.com>
2025-09-10 20:50:33 -0300

    Add files via upload

---
+++ Exercícios de Operadores em Python.py
@@ -0,0 +1,33 @@
+#1Calcular a soma de 15 + 27
+a = 15
+b = 27
+soma= (a+b)
+print (soma)
+
+#2Multiplique 12 por 8
+
+c = 12
+d = 8
+Multiplicação= (c*d)
+print (Multiplicação)
+
+#3Divide 25 por 4 e veja o resultado
+
+e = 25
+f = 4
+Divisão= (e/f)
+print (Divisão)
+
+#4Descubra o resto da divisão de 37 por 5
+
+g = 37
+h = 5
+resto= (g%h)
+print (resto)
+
+#5Eleve 3 à potência de 4
+
+i = 3
+j = 4
+potencia= (i**j)
+print (potencia)
