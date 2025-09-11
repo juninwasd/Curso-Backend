0aab7aaaa6e782060ab73866b18d5ba2a4e9c890
junin <juniomoreirabrito@gmail.com>
2025-09-10 21:16:20 -0300

    Update Exercícios de Operadores em Python.py

--- Exercícios de Operadores em Python.py
+++ Exercícios de Operadores em Python.py
@@ -1,36 +1,37 @@
 #1Calcular a soma de 15 + 27
+
 a = 15
 b = 27
 soma= (a+b)
-print ('resultado1:', soma)
+print ('Resultado pergunta 1.:', soma)
 
 #2Multiplique 12 por 8
 
 c = 12
 d = 8
 Multiplicação= (c*d)
-print ('resultado2:', Multiplicação)
+print ('Resultado pergunta 2.:', Multiplicação)
 
 #3Divide 25 por 4 e veja o resultado
 
 e = 25
 f = 4
 Divisão= (e/f)
-print ('resultado3:', Divisão)
+print ('Resultado pergunta 3.:', Divisão)
 
 #4Descubra o resto da divisão de 37 por 5
 
 g = 37
 h = 5
 resto= (g%h)
-print ('resultado4:', resto)
+print ('Resultado pergunta 4.:', resto)
 
 #5Eleve 3 à potência de 4
 
 i = 3
 j = 4
 potencia= (i**j)
-print ('resultado5:', potencia)
+print ('Resultado pergunta 5.:', potencia)
 
 #6Verifique se 10 é maior que 7
 
@@ -38,7 +39,7 @@ k = 10
 l = 7
 
 Verifique= (k > l)
-print ('resultado6:',Verifique)
+print ('Resultado pergunta 6.:',Verifique)
 
 #7Verifique se 15 é igual a 20
 
@@ -46,7 +47,7 @@ m = 10
 n = 7
 
 Verifique2= (m == n)
-print ('resultado7:',Verifique2)
+print ('Resultado pergunta 7.:',Verifique2)
 
 #8Teste se 100 é menor ou igual a 100
 
@@ -54,7 +55,7 @@ o = 100
 p = 100
 
 Verifique2= (o <= p)
-print ('resultado8:',Verifique2)
+print ('Resultado pergunta 8.:',Verifique2)
 
 #9Veja se 45 é diferente de 30
 
@@ -62,4 +63,4 @@ q = 45
 r = 30
 
 Verifique2= (q != p)
-print ('resultado9:',Verifique2)
+print ('Resultado pergunta 9.:',Verifique2)
