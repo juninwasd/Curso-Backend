477b473546e887076b2a2f1ebc159b0aed211ebe
junin <juniomoreirabrito@gmail.com>
2025-09-10 21:50:20 -0300

    Update Exercícios de Operadores em Python.py

--- Exercícios de Operadores em Python.py
+++ Exercícios de Operadores em Python.py
@@ -1,66 +1,107 @@
-#1Calcular a soma de 15 + 27
+#1 Calcular a soma de 15 + 27
 
-a = 15
+a= 15
 b = 27
 soma= (a+b)
 print ('Resultado pergunta 1.:', soma)
 
-#2Multiplique 12 por 8
+#2 Multiplique 12 por 8
 
 c = 12
 d = 8
 Multiplicação= (c*d)
 print ('Resultado pergunta 2.:', Multiplicação)
 
-#3Divide 25 por 4 e veja o resultado
+#3 Divide 25 por 4 e veja o resultado
 
 e = 25
 f = 4
 Divisão= (e/f)
 print ('Resultado pergunta 3.:', Divisão)
 
-#4Descubra o resto da divisão de 37 por 5
+#4 Descubra o resto da divisão de 37 por 5
 
 g = 37
 h = 5
 resto= (g%h)
 print ('Resultado pergunta 4.:', resto)
 
-#5Eleve 3 à potência de 4
+#5 Eleve 3 à potência de 4
 
 i = 3
 j = 4
 potencia= (i**j)
 print ('Resultado pergunta 5.:', potencia)
 
-#6Verifique se 10 é maior que 7
+#6 Verifique se 10 é maior que 7
 
 k = 10
 l = 7
 
-Verifique= (k > l)
-print ('Resultado pergunta 6.:',Verifique)
+Maior= (k > l)
+print ('Resultado pergunta 6.:',Maior)
 
-#7Verifique se 15 é igual a 20
+#7 Verifique se 15 é igual a 20
 
 m = 10
 n = 7
 
-Verifique2= (m == n)
-print ('Resultado pergunta 7.:',Verifique2)
+Igual= (m == n)
+print ('Resultado pergunta 7.:',Igual)
 
-#8Teste se 100 é menor ou igual a 100
+#8 Teste se 100 é menor ou igual a 100
 
 o = 100
 p = 100
 
-Verifique2= (o <= p)
-print ('Resultado pergunta 8.:',Verifique2)
+Menor_ou_igual = (o <= p)
+print ('Resultado pergunta 8.:',Menor_ou_igual)
 
-#9Veja se 45 é diferente de 30
+#9 Veja se 45 é diferente de 30
 
 q = 45
 r = 30
 
-Verifique2= (q != p)
-print ('Resultado pergunta 9.:',Verifique2)
+Diferente= (q != p)
+print ('Resultado pergunta 9.:',Diferente)
+
+#10 Verifique se 5>2 e 8>3
+
+s = 5
+t = 2 
+u = 8
+v = 3
+
+AND= (s>t and u>v)
+print ('Resultado pergunta 10.:',AND)
+
+#11 Verifique se 10<3 ou 4==4
+
+w = 10
+x = 3 
+y = 4
+z = 3
+
+OR= (w>x or y>z)
+print ('Resultado pergunta 11.:',OR)
+
+#12 Inverta o resultado lógico de (7>2) usando not
+
+aa = 7
+ab = 2
+
+NOT= not(aa>ab)
+print ('Resultado pergunta 12.:',NOT)
+
+#13 Crie uma Variável x com valor 10. Depois faça:
+
+X = 10
+
+questão_1= (X + 5)
+print ('Parte 1.:',questão_1)
+
+questão_2= (questão_1 * 2)
+print ('Parte 2.:',questão_2)
+
+questão_3= (questão_2 - 4)
+print ('Parte Total.:',questão_3)
