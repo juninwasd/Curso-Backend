23f72c9937eac207ce2685d973adefcfab01c0e3
junin <juniomoreirabrito@gmail.com>
2025-09-10 21:05:41 -0300

    Update Exercícios de Operadores em Python.py

--- Exercícios de Operadores em Python.py
+++ Exercícios de Operadores em Python.py
@@ -2,32 +2,64 @@
 a = 15
 b = 27
 soma= (a+b)
-print (soma)
+print ('resultado1:', soma)
 
 #2Multiplique 12 por 8
 
 c = 12
 d = 8
 Multiplicação= (c*d)
-print (Multiplicação)
+print ('resultado2:', Multiplicação)
 
 #3Divide 25 por 4 e veja o resultado
 
 e = 25
 f = 4
 Divisão= (e/f)
-print (Divisão)
+print ('resultado3:', Divisão)
 
 #4Descubra o resto da divisão de 37 por 5
 
 g = 37
 h = 5
 resto= (g%h)
-print (resto)
+print ('resultado4:', resto)
 
 #5Eleve 3 à potência de 4
 
 i = 3
 j = 4
 potencia= (i**j)
-print (potencia)
+print ('resultado5:', potencia)
+
+#6Verifique se 10 é maior que 7
+
+k = 10
+l = 7
+
+Verifique= (k > l)
+print ('resultado6:',Verifique)
+
+#7Verifique se 15 é igual a 20
+
+m = 10
+n = 7
+
+Verifique2= (m == n)
+print ('resultado7:',Verifique2)
+
+#8Teste se 100 é menor ou igual a 100
+
+o = 100
+p = 100
+
+Verifique2= (o <= p)
+print ('resultado8:',Verifique2)
+
+#9Veja se 45 é diferente de 30
+
+q = 45
+r = 30
+
+Verifique2= (q != p)
+print ('resultado9:',Verifique2)
