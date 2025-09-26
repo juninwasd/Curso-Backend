f4fbdc184ea1b75668b6c0c2d1960e40b3d64e3f
junin <juniomoreirabrito@gmail.com>
2025-09-25 21:43:14 -0300

    Add files via upload

--- manipulacao_de_arquivo.py
+++ manipulacao_de_arquivo.py
@@ -32,22 +32,22 @@ with open("nomes.txt", "w") as arq:
 
 with open("novo.txt", "x") as arq:
     arq.write("arquivo novo")
+
+with open("usuario.txt", "r") as arq:
+    linhas = arq.readlines()
+
+linhas_formadas =[]
+for linha in linhas:
+    nome, idade = linha.strip().split(";")
+    linhas_formadas.append(f"{nome.upper()};{idade}\n")
+
+with open("usuario.txt", "w") as arq:
+    arq.writelines(linhas_formadas)
 '''
 
-with open("usuarios.txt","r") as arq:
-    linhas = arq.readline()
-
-linhas_formatadas = []
-for linha in linhas:
-    nome,idade  = linha.strip().split(",")
-    linhas_formatadas.append(f"{nome.upper()};{idade}\n")
-
-with open("usuarios.txt","w") as arq:
-    arq.writelines(linhas_formatadas)
-
-
-
-
-
-
+try:
+    with open("log.txt", "x") as arq:
+        arq.write("Arquivo criado com sucesso!\n")
+except FileExistsError:
+    print("O arquivo log.txt já existe")
 
